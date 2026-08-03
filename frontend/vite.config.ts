c136d5da322e7d5394f471afe44abfb47e9ab033
Asad Ali <asadali@cyberzeusglobal.com>
2026-08-03 19:58:32 +0500

    docker build image time reduce

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
   server: {
-    port: 3000,
+    port: 3001,
     strictPort: true,
 
     proxy: {
